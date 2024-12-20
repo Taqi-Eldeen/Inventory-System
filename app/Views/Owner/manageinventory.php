4e9296b72aab881b5dfa41d88af3da10af0e2e2b
TaqiEldeen <168198086+Taqi-Eldeen@users.noreply.github.com>
2024-12-20 19:08:32 +0200

    Fixed Includes

--- app/Views/Owner/manageinventory.php
+++ app/Views/Owner/manageinventory.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<?php require 'sidebarowner.php'; ?>
+<?php include '../User/sidebar.php'; ?>
 
 <div class="main-content">
     <h2>Business Owner Inventory</h2>
